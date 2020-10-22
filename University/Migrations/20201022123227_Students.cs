fb1d98f0f1eb05b4719fabed04a724a9cb1b35fc
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2020-10-22 15:23:24 +0200

    Add Seed

---
+++ University/Migrations/20201022123227_Students.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace University.Migrations
+{
+    public partial class Students : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Adress_Student_StudentId",
+                table: "Adress");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Student_StudentId",
+                table: "Enrollment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Student",
+                table: "Student");
+
+            migrationBuilder.RenameTable(
+                name: "Student",
+                newName: "Students");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Adress_Students_StudentId",
+                table: "Adress",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Students_StudentId",
+                table: "Enrollment",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Adress_Students_StudentId",
+                table: "Adress");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Students_StudentId",
+                table: "Enrollment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.RenameTable(
+                name: "Students",
+                newName: "Student");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Student",
+                table: "Student",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Adress_Student_StudentId",
+                table: "Adress",
+                column: "StudentId",
+                principalTable: "Student",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Student_StudentId",
+                table: "Enrollment",
+                column: "StudentId",
+                principalTable: "Student",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
