36e00e939ec5be77e582015b3497c9277ef5d091
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2020-10-23 10:39:02 +0200

    A tag first try

---
+++ University/Models/ViewModels/StudentAddViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University.Models.ViewModels
+{
+    public class StudentAddViewModel
+    {
+        //public string FirstName { get; set; }
+        //public int MyProperty { get; set; }
+    }
+}
---
+++ University/TagHelpers/ATagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes ="action", ParentTag ="p")]
+    public class ATagHelper : TagHelper
+    {
+
+        public string Action { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public IUrlHelperFactory urlHelperFactory { get; }
+
+        public ATagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            this.urlHelperFactory = urlHelperFactory;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            output.Attributes.Add("href", $"../Students/{Action}");
+        }
+
+    }
+}
