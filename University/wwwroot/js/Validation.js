273d90f8f2670c5ecc52202cd37f998b07487600
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2020-10-23 13:16:56 +0200

    Add JsValidation

--- University/wwwroot/js/Validation.js
+++ University/wwwroot/js/Validation.js
@@ -1,1 +1,7 @@
-﻿
+﻿$.validator.addMethod('streetnr', function (value, element, params) {
+    let last = value.split('').pop();
+    let res = parseInt(last);
+    return isNaN(res) ? false : true;
+});
+
+$.validator.unobtrusive.adapters.addBool('streetnr');
